d7675ded4d9a75c233a2f94a66cd73e5d7c6049a
nhan <nhanng19@gmail.com>
2022-10-17 03:00:07 -0700

    added recipe

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const getFood = () => {
+const submit = document.getElementById('submit')
+
+submit.onclick = getFood = () => {
 var myHeaders = new Headers();
 myHeaders.append("apikey", "I1v5mLVa9E2WwsnrKGzY0mVoEpGHTE62");
 const query = document.getElementById('input').value
@@ -9,11 +11,15 @@ var requestOptions = {
 };
 
 fetch(`https://api.apilayer.com/spoonacular/food/ingredients/search?sortDirection=asc&sort=calories&query=${query}&offset=0&number=5&intolerances=peanut`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+.then((response) => response.json())
+.then((data) => {
+    for (let i = 0; i < 4; i++){
+    const recipe = data.results[i].name;
+    const recipes = $('.recipe');
+    recipes[i].innerHTML = recipe;
+    recipes[i].style.display = "block";
+    }
+})
 }
-
-
 
 
